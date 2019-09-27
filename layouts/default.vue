ee7815249111c465c34c6c487a3bbd82f1a4bb19
romain sanson <romainsanson.gm@gmail.com>
2019-09-27 23:06:58 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <app-nav></app-nav>
+    <div class="container">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav.vue'
+export default {
+  components: {
+    appNav: Nav
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/documentation/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est aliquid quis quod, qui expedita, modi saepe omnis ipsa ipsum architecto quibusdam sed. Velit minima nulla, in rerum consequatur quis ducimus laboriosam aut. Tenetur dolore ratione, quas incidunt hic. Commodi molestias vero facere velit officia adipisci, blanditiis ipsa provident veniam ad.</p>
+    <br>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi perspiciatis, hic quod esse nulla rem aperiam odit dolor sunt placeat quibusdam harum nam, iusto eum, ipsa mollitia adipisci commodi impedit omnis reiciendis quasi possimus unde autem, cum. Nisi sed, doloribus ab sint officiis eum. Nisi ducimus culpa laudantium ratione eaque saepe distinctio placeat animi ipsam atque expedita fugiat nobis, accusamus accusantium. Sed, explicabo. Soluta nostrum quisquam modi doloremque, debitis dolorum quam iure veniam perspiciatis laborum nihil asperiores expedita incidunt sit libero distinctio magni, impedit sapiente accusamus magnam sint iusto autem eaque molestias? Consectetur corrupti fuga perferendis provident tenetur tempora dolor.</p>
+    <br>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint beatae voluptas fugiat nesciunt quam mollitia sapiente odio minus dolores qui nostrum commodi nemo, consequuntur cum voluptates, atque dolorum consectetur ducimus porro, hic placeat blanditiis fugit? Eaque accusamus voluptas ducimus molestias nihil. Ea atque ipsam quaerat esse ipsa aperiam praesentium vitae.</p>
+  </div>
+</template>
